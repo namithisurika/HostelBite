2472f0015ebb3e2dbc8008522cd272b1fd635000
Sithum Raigamage <sraig2002@gmail.com>
2025-10-28 22:47:03 +0530

    feat(init): initial files for the project

---
+++ HostelBite/HostelBite/HostelBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostelBiteApp.swift
+//  HostelBite
+//
+//  Created by Sithum Raigamage on 2025-10-28.
+//
+
+import SwiftUI
+
+@main
+struct HostelBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
