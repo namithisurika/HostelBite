1b56366f08855533877a4f8b0a1b258d046fd771
Sithum Raigamage <sraig2002@gmail.com>
2025-10-29 18:15:00 +0530

    feat(view): basic view

--- HostelBite/HostelBite/Views/ContentView.swift
+++ HostelBite/HostelBite/Views/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background color
+            Color.orange
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                // App title
+                Text("🍴 HostelBite")
+                    .font(.system(size: 50, design: .serif))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                // App description
+                Text("Organize weekly meals with your roommates")
+                    .font(.system(size: 20, weight: .light, design: .default))
+                    .font(.subheadline .monospaced())
+                    .foregroundColor(.white.opacity(0.9))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 5)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                // Developers' names
+                Text("Developed by: Sithum Raigamage, Namith ,Sanuth")
+                    .font(.footnote)
+                    .foregroundColor(.white.opacity(0.8))
+                    .padding(.bottom, 20)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
